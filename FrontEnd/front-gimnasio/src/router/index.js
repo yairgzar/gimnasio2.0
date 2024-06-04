00fe45df02bd603905cf800259d27fdd32fcda38
Yair Gonzalez Salazar <210256@utxicotepec.edu.mx>
2024-06-03 18:21:48 -0600

    Creacion del SideBar

--- FrontEnd/front-gimnasio/src/router/index.js
+++ FrontEnd/front-gimnasio/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../components/login.vue'
 import RegisterUserView from '../components/RegisterUser.vue'
+import menuView from '../components/menu.vue'
  
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterUserView
+    },
+    {
+      path: '/menu',
+      name: 'menu',
+      component: menuView
     }
     
   ]
