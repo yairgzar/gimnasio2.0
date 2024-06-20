2210d1865a0a2443c0d2b37996ae9f7773d21ed2
Yair Salazar <210256@utxicotepec.edu.mx>
2024-06-20 11:48:02 -0600

    Agregar usuarios con API sin BD

--- BackEnd/routes/user.py
+++ BackEnd/routes/user.py
@@ -1,8 +1,30 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from datetime import datetime
 
 user = APIRouter()
+users = []
+
+#usersModel
+class model_user(BaseModel):
+    id:str
+    usuario:str
+    contrasena: str
+    created_at:datetime = datetime.now()
+    estatus:bool=False
+@user.get("/")    
+
+def bienvenida ():
+    return "Bienvenido al sistema de APIS"
 
 @user.get("/users")
 
-def helloworld():
-    return "Hola 9B"
+def get_usuarios():
+    return users
+
+@user.post("/users")
+
+def save_usuarios(insert_users:model_user):
+    users.append(insert_users)
+    #print(users)
+    return "Datos guardados"
