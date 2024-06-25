c6ffa4b3971b94b71899d58022e25d47e4bd2566
Yair Salazar <210256@utxicotepec.edu.mx>
2024-06-25 10:28:32 -0600

    Se agrego el model_persona

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from routes.user import user 
+from routes.persona import person 
 
 app=FastAPI()
 app.include_router(user)
+app.include_router(person)
 print ("Hola bienvenido a mi backend")
--- BackEnd/routes/user.py
+++ BackEnd/routes/user.py
@@ -14,20 +14,32 @@ class model_user(BaseModel):
     created_at: datetime = datetime.now()
     estatus: bool = False
 
+# Person Model
+class model_person(BaseModel):
+    id: int
+    nombre: str
+    primer_apellido: str
+    segundo_apellido: str
+    titulo_cortesia: str
+    fecha_nacimiento: str
+    genero: str
+    tipo_sangre: str
+
+
 @user.get("/")
 def bienvenida():
     return "Bienvenido al sistema de apis"
 
-@user.get("/users")
+@user.get("/users", tags=["Usuarios"])
 def get_usuarios():
     return users
 
-@user.post("/users")
+@user.post("/users", tags=["Usuarios"])
 def save_usuarios(insert_users: model_user):
     users.append(insert_users)
     return "Datos guardados"
 
-@user.put("/users/{user_id}")
+@user.put("/users/{user_id}", tags=["Usuarios"])
 def update_usuario(user_id: str, updated_user: model_user):
     for index, user in enumerate(users):
         if user.id == user_id:
@@ -35,7 +47,7 @@ def update_usuario(user_id: str, updated_user: model_user):
             return {"message": "Usuario actualizado"}
     raise HTTPException(status_code=404, detail="Usuario no encontrado")
 
-@user.delete("/users/{user_id}")
+@user.delete("/users/{user_id}" , tags=["Usuarios"])
 def delete_usuario(user_id: str):
     for index, user in enumerate(users):
         if user.id == user_id:
