f3fb647feabc63f05106874ea4bc4b6d97e4e6e7
Yair Salazar <210256@utxicotepec.edu.mx>
2024-07-04 11:19:30 -0600

    CRUD Personas con MySQL

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -1,11 +1,9 @@
 from fastapi import FastAPI
 from routes.user import user 
-from routes.persona import person 
+from routes.person import person 
 
 app=FastAPI(
-    title="Gimnasio S.A. de C.V.",
-    description="API para el almacenamiento de información de un gimnasio"
-)
+    )
 app.include_router(user)
 app.include_router(person)
 print ("Hola bienvenido a mi backend")
